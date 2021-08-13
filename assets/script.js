937116fd1eb829ab1e352ee5065de1ba0932c0d2
Zachary Dowd <zdowd2796@gmail.com>
2021-08-12 19:08:13 -0600

    date added

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,35 @@
 var hourEl = $('.hour');
 var inputEl = $('textarea');
 var saveEl = $('.saveBtn');
-var timeEl = moment().format('HA');
-var timeslots = ["9AM", "10AM", "11AM", "12PM","1PM", "2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM" ];
+var slot1El = $('.slot1')
+var timeEl = moment().hour()
+var timeslots = ["9", "10", "11", "12","13", "14","15","16","17","18","19","20","21" ];
+var slot1 = timeslots[0];
+var slot2 = timeslots[1];
+var slot3 = timeslots[2];
+var slot4 = timeslots[3];
+var slot5 = timeslots[4];
+var slot6 = timeslots[5];
+var slot7 = timeslots[6];
+var slot8 = timeslots[7];
+var slot9 = timeslots[8];
+var slot10 = timeslots[9];
+var slot11 = timeslots[10];
+var slot12 = timeslots[11];
 //displays current day
 $('#currentDay').html(moment().format("MMM DD YYYY"));
 init();
 
 function init(){
     generateTimes();
-    storeData();
+    // storeData();
 }
 
 
-function generateTimes(){
 
+console.log(timeEl)
+console.log(timeEl > slot1)
+// function storeData(){
 
-    for (let i = 0; i < timeslots.length; i++) {
-        const slotsEl = timeslots[i];
-        var time = document.createElement('p');
-        time.textContent = slotsEl;
-        hourEl[i].appendChild(time);
-        if(timeEl > slotsEl){
-            
-        }
-    }
-}
+// }
 
-function storeData(){
-
-}
-
-console.log(hourEl)
