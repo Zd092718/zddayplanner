d0b8465107ccdd6f602ba997ab45f5d227588b06
Zachary Dowd <zdowd2796@gmail.com>
2021-08-17 07:00:48 -0600

    color associations funcioning

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,30 @@
 var hourEl = $('.hour');
 var inputEl = $('textarea');
 var saveEl = $('.saveBtn');
-var slot1El = $('.slot1')
 var timeEl = moment().hour()
-var timeslots = ["9", "10", "11", "12","13", "14","15","16","17","18","19","20","21" ];
-var slot1 = timeslots[0];
-var slot2 = timeslots[1];
-var slot3 = timeslots[2];
-var slot4 = timeslots[3];
-var slot5 = timeslots[4];
-var slot6 = timeslots[5];
-var slot7 = timeslots[6];
-var slot8 = timeslots[7];
-var slot9 = timeslots[8];
-var slot10 = timeslots[9];
-var slot11 = timeslots[10];
-var slot12 = timeslots[11];
+
+
 //displays current day
 $('#currentDay').html(moment().format("MMM DD YYYY"));
-init();
 
-function init(){
-    generateTimes();
-    // storeData();
-}
+saveEl.on('click', function(){
+    let textarea = $(this).siblings().eq(1).attr('id')
+})
+
+$('textarea').each(function () {
+    var rowHour = parseInt($(this).attr('id').split('-')[1])
+    console.log(rowHour)
+    var sameId = $(this).attr('id');
+    var task = localStorage.getItem(sameId);
+    if(timeEl > rowHour){
+        console.log("hit")
+        $(this).addClass('past')
+    } else if (timeEl === rowHour){
+        $(this).addClass('present')
+    } else {
+        $(this).addClass('future')
+    }
+})
 
 
-
-console.log(timeEl)
-console.log(timeEl > slot1)
-// function storeData(){
-
-// }
 
